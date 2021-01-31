792b2434d7bb1e29bbf3eaa6f589482ce8f34c72
Benoit Marzloff <78317471+BenoitMarzloff@users.noreply.github.com>
2021-01-31 20:28:04 +0100

    Add files via upload

---
+++ 1st One.py
@@ -0,0 +1,17 @@
+# This program says hello and asks for my name
+
+print('Hello');
+
+name = input('''What's your name?
+''')
+
+print('Your name is ',name)
+
+print('Your name is ',len(name), ' characters long. ')
+
+
+age = input('How old are you? ');
+
+print('You are ',age,' years old');
+nextage = int(age) + 1
+print('You will be ',nextage, ' years old next year')
