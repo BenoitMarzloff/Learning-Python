aed557712f1a3afb6eb8b0d32c55356c92a08cb7
Benoit Marzloff <78317471+BenoitMarzloff@users.noreply.github.com>
2021-02-02 13:31:49 +0100

    Add files via upload
    Here I define my own function based on a really bad joke. I also convert the given name in capital letters when appropriate.

---
+++ KnockKnock.py
@@ -0,0 +1,16 @@
+name = input('Enter your name: ')
+
+def KnockKnock():
+    print('Knock knock')
+    print('''Who's there?''')
+    print('Dad.')
+    print('Dad who?')
+    print('UNLOCK THE DOOR '+ name.upper())
+
+KnockKnock()
+KnockKnock()
+KnockKnock()
+
+
+print()
+print('Okay ' + name +' stop this python program this is torture.')
